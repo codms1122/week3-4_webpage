f14190379c9452c1bc84ead70da6e0b8cb0e6dc0
codms1122 <hanmango2000@gmail.com>
2024-12-14 21:50:22 +0900

    [ADD] 리스트 조회 sql

--- app.py
+++ app.py
@@ -20,22 +20,55 @@ def get_db_connection():
 
 
 # 임시 데이터 저장
-posts = []
+sql_select = []
+#posts_cnt = []
 
 
-@app.route('/post')
+@app.route('/post', methods=['GET', 'POST'])
 def post_list():
-    connection = get_db_connection()  # DB 연결
-    cursor = connection.cursor(dictionary=True)  # dictionary 형식으로 결과 반환
+    #DB연결
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)  # dictionary로 결과 반환하도록
 
-    sql='SELECT * FROM testpost' ##### ❗❗❗❗ DB명 변경 ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
+    #검색조건(post_query) 받아오기
+    query_type = request.form.get('query_type') 
+    query_content = request.form.get('query_content')
+
+    if query_content:
+        if query_type=="title":
+            sql = 'SELECT postId, postTitle, DATE(createdTime) AS createdTime FROM testpost WHERE postTitle LIKE "' + query_content + '"'
+            
+    else:
+        print("검색어 없음")
+
+    #전체 리스트 조회
+    sql='select postId, postTitle, date(createdTime) AS createdTime from testpost' ##### ❗❗❗❗ DB명 변경 ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
     cursor.execute(sql)  # testpost 테이블의 모든 데이터 가져오기
-    posts = cursor.fetchall()  # 데이터 모두 가져오기
+    sql_select = cursor.fetchall()  # posts 리스트에 데이터 담기
 
+    #개수세기
+    sql='select count(*) AS count from testpost' ### ❗❗❗❗ DB명 변경 ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
+    cursor.execute(sql)
+    sql_count = cursor.fetchall()
+    sql_count = sql_count[0]['count']
+    
+    
+    # DB 연결 종료
     cursor.close()
-    connection.close()  # DB 연결 종료
+    connection.close()  
 
-    return render_template('post.html', post_list_data=posts)  # HTML 템플릿에 데이터 전달
+
+    print("========================\n")
+    print("log!! \n")
+    print("Fetched posts data from DB:")
+    print(sql)
+    print(sql_select)
+    print(sql_count)  
+    print(query_type,query_content)
+    print("\n========================")
+
+
+    return render_template('post.html', post_cnt=sql_count, post_list=sql_select)  # HTML 템플릿에 데이터 전달  
 
 
 
