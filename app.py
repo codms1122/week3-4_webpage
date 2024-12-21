31404aa78d5d3961437cafc2b8b890df26ea863a
codms1122 <hanmango2000@gmail.com>
2024-12-21 23:08:50 +0900

    [ADD] 회원가입

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import mysql.connector
 from config import Config #DB 정보 저장된 파일일
 
@@ -24,7 +24,6 @@ def post():
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True) 
 
-
     #검색조건(post_query) 받아오기
     query_type = request.form.get('query_type')
     query_content = request.form.get('query_content')
@@ -33,13 +32,13 @@ def post():
     #검색조건에 맞게 sql질의
     if query_content:    
         if query_type=="title":
-            sql = 'SELECT postId, postTitle, DATE(createdTime) AS createdTime FROM testpost WHERE postTitle LIKE "%' + query_content + '%"'
+            sql = 'SELECT postId, postTitle, DATE(createdTime) AS createdTime FROM post WHERE postTitle LIKE "%' + query_content + '%"'
         elif query_type=="content":
-            sql = 'SELECT postId, postTitle, DATE(createdTime) AS createdTime FROM testpost WHERE postContent LIKE "%' + query_content + '%"'
+            sql = 'SELECT postId, postTitle, DATE(createdTime) AS createdTime FROM post WHERE postContent LIKE "%' + query_content + '%"'
         else:
-            sql = 'SELECT postId, postTitle, DATE(createdTime) AS createdTime FROM testpost WHERE postTitle LIKE "%' + query_content + '%" or postContent LIKE "%' + query_content + '%"'
+            sql = 'SELECT postId, postTitle, DATE(createdTime) AS createdTime FROM post WHERE postTitle LIKE "%' + query_content + '%" or postContent LIKE "%' + query_content + '%"'
     else:
-        sql='select postId, postTitle, date(createdTime) AS createdTime from testpost'
+        sql='select postId, postTitle, date(createdTime) AS createdTime from post'
     
     #sql질의 결과 담기
     cursor.execute(sql)  
@@ -61,7 +60,7 @@ def post_detail(post_id):
     cursor = connection.cursor(dictionary=True)
 
     sql = ('SELECT postId, postTitle, postContent, createdTime, lastModifiedTime '
-           'FROM testpost WHERE postId = ' + str(post_id))
+           'FROM post WHERE postId = ' + str(post_id))
     cursor.execute(sql)  
     sql_select = cursor.fetchall()
     sql_select = sql_select[0]
@@ -83,7 +82,7 @@ def post_create():
         if create_title and create_content:
             state = "create"
             sql = (
-                'INSERT INTO testpost (postTitle, postContent) '
+                'INSERT INTO post (postTitle, postContent) '
                 'VALUES ("' + create_title + '", "' + create_content + '");'
             )
             cursor.execute(sql)
@@ -102,10 +101,6 @@ def post_create():
 
 
 
-
-
-
-
 @app.route('/post/update/<int:post_id>', methods=['GET', 'POST'])
 def post_update(post_id):
     #DB연결
@@ -113,7 +108,7 @@ def post_update(post_id):
     cursor = connection.cursor(dictionary=True)
 
     sql = ('SELECT postId, postTitle, postContent, createdTime, lastModifiedTime '
-           'FROM testpost WHERE postId = ' + str(post_id))
+           'FROM post WHERE postId = ' + str(post_id))
     cursor.execute(sql)  
     sql_select = cursor.fetchall()
     sql_select = sql_select[0]
@@ -125,7 +120,7 @@ def post_update(post_id):
     if request.method == 'POST':
         if update_title and update_content:
             state = "update"
-            sql = ('UPDATE testpost '
+            sql = ('UPDATE post '
                    'SET postTitle = "'+ update_title +'", postContent = "'+ update_content +'", lastModifiedTime = NOW() '
                    'WHERE postId = ' + str(post_id))
             cursor.execute(sql)
@@ -144,9 +139,6 @@ def post_update(post_id):
 
 
 
-
-
-
 @app.route('/post/delete/<int:post_id>', methods=['GET', 'POST'])
 def post_delete(post_id):
     #DB연결
@@ -160,7 +152,7 @@ def post_delete(post_id):
         print(delete_yn)
         if delete_yn == "yes":
             state = "delete"
-            sql = 'DELETE FROM testpost WHERE postId = ' + str(post_id)
+            sql = 'DELETE FROM post WHERE postId = ' + str(post_id)
             cursor.execute(sql)  
             connection.commit()
             return render_template('next.html',state=state)
@@ -174,7 +166,65 @@ def post_delete(post_id):
 
 
 
+@app.route('/login') #, methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
+
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+
+    #DB연결
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+
+    #form 내용 가지고오기
+    btn_action = request.form.get("btn_action")
+    user_id = request.form.get('user_id')
+    user_email = request.form.get('user_email')
+    user_name = request.form.get('user_name')
+    user_pw = request.form.get('user_pw')
+    user_orgn = request.form.get('user_orgn')
+    user_major = request.form.get('user_major')
+
+    if btn_action == "submit":
+        state = "join"
+        sql = (
+            'INSERT INTO user (userId, userEmail, userName, userPw, userOrgn, userMajor) '
+            'VALUES ("' + user_id + '", "' + user_email + '", "' + user_name + '", "' + user_pw + '", "' + user_orgn + '", "' + user_major + '");'
+        )
+        cursor.execute(sql)
+        connection.commit()
+        return render_template('next.html',state=state)
+
+    return render_template('join.html')
+
+
+
+@app.route('/id_chk', methods=['POST']) #'GET', 
+def check_id():
+    user_id = request.form.get('user_id')
+
+    #DB연결
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+
+    #id 중복 확인 쿼리
+    sql = 'SELECT count(*) AS cnt FROM user WHERE userId = "' + user_id + '"'
+    cursor.execute(sql)
+    sql_cnt = cursor.fetchone()
+    sql_cnt = sql_cnt['cnt']
+
+    if sql_cnt == 0:
+        return jsonify({'id_chk': 'not_exists', "id_msg": "사용 가능한 아이디입니다."})
+    else:
+        return jsonify({'id_chk': 'exists', "id_msg": "사용할 수 없는 아이디입니다. 다른 아이디를 입력해 주세요."})
+
+
+@app.route('/test')
+def test():
+    state = "create"
+    return render_template('next.html',state=state)
 
 
 if __name__ == '__main__':
